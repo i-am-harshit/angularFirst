fa30329adbf2d1a5c8185ed830e2b5f6de334096
harshit <i.am.hrshit>
2023-08-10 10:42:48 +0530

    create folders

--- src/app/MyComponents/side-bar/side-bar.component.spec.ts
+++ src/app/MyComponents/side-bar/side-bar.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { SideBarComponent } from './side-bar.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SideBarComponent', () => {
+  let component: SideBarComponent;
+  let fixture: ComponentFixture<SideBarComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [SideBarComponent]
     });
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SideBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './MyComponents/homepage/homepage.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { SideBarComponent } from './MyComponents/side-bar/side-bar.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    SideBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
